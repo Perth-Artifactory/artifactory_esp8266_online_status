bdb1b49ee87bc440915c7c26ca4aae234d86314a
tazard <16077190+tazard@users.noreply.github.com>
2021-10-27 20:44:02 +0800

    Remove LEDs, modernise to library updates Also made more user friendly.

--- artifactory_esp8266_online_status.ino
+++ artifactory_esp8266_online_status.ino
@@ -1,30 +1,22 @@
 /**
- * Artifactory ESP8266 Online Status
+ * ESP8266 Online Status
  * by Blake Samuels aka Tazard
  * 
  * Minimal modification of BasicHTTPClient.ino for use as
  * online status tool using statuscake. GETs a URL at 
- * statuscake every 60s to report if Artifactory internet 
+ * statuscake every 10s to report if Artifactory internet 
  * is online at the space.
  * 
- * Details on getting SHA1 fingerprint so can connect to 
- * HTTPS:
- * https://forum.arduino.cc/index.php?topic=515541.0
- * https://github.com/esp8266/Arduino/issues/2556
- * 
- * Those posts gave these commands (on linux host):
- *  openssl s_client -connect push.statuscake.com:443
- *  nano statuscake-cert.pem (paste key in here)
- *  openssl x509 -noout -in statuscake-cert.pem -fingerprint -sha1
- * Get end bit of:
- *  SHA1 Fingerprint=07:ED:A2:C1:4C:E6:76:FA:C6:F7:02:B3:52:8C:AE:5A:9D:01:96:1D
- * 
- * TODO: SHA1 key might change. ESP32 bsides work showed ESP32 can take whole 
- * certificate and verify it. If SHA1 key changes on statuscake then see if 
- * ESP8266 can do this or change to ESP32.
- *
- * Date 2019-11-02
+ * Major Update, Date 2021-10-27
 */
+
+// Set WIFI details
+const char* const SSID = "Artifactory";
+const char* const PASSWORD = "KEEP DOOR CLOSED";
+
+// Set statuscake URL
+const char* const URL = "https://push.statuscake.com/?PK=3dc5296d8883eda&TestID=5273311&time=0";
+
 
 #include <Arduino.h>
 
@@ -33,115 +25,69 @@
 
 #include <ESP8266HTTPClient.h>
 
-#define USE_SERIAL Serial
+#include <WiFiClientSecureBearSSL.h>
 
 ESP8266WiFiMulti WiFiMulti;
 
-// Set statuscake URL and SHA1 fingerprint here (as details above)
-const char* url = "https://push.statuscake.com/?PK=3dc5296d8883eda&TestID=5273311&time=0";
-const char* fingerprint = "07:ED:A2:C1:4C:E6:76:FA:C6:F7:02:B3:52:8C:AE:5A:9D:01:96:1D";
-
-const int pinR = D1;
-const int pinB = D2;
-const int pinG = D3;
-
-void ledOff() {
-  digitalWrite(pinB, HIGH);
-  digitalWrite(pinG, HIGH);
-  digitalWrite(pinR, HIGH);
-}
-
-void ledWhite() {
-  digitalWrite(pinB, LOW);
-  digitalWrite(pinG, LOW);
-  digitalWrite(pinR, LOW);
-}
-
-void ledRed() {
-  digitalWrite(pinB, HIGH);
-  digitalWrite(pinG, HIGH);
-  digitalWrite(pinR, LOW);
-}
-
-void ledGreen() {
-  digitalWrite(pinB, HIGH);
-  digitalWrite(pinG, LOW);
-  digitalWrite(pinR, HIGH);
-}
-
-void ledBlue() {
-  digitalWrite(pinB, LOW);
-  digitalWrite(pinG, HIGH);
-  digitalWrite(pinR, HIGH);
-}
-
 void setup() {
-  USE_SERIAL.println("Artifactory ESP8266 Online Status");
-  USE_SERIAL.flush();
 
-  pinMode(pinB, OUTPUT);
-  pinMode(pinG, OUTPUT);
-  pinMode(pinR, OUTPUT);
+  Serial.begin(115200);
+  // Serial.setDebugOutput(true);
 
-  ledWhite(); delay(1000);
-  ledRed();   delay(1000);
-  ledGreen(); delay(1000);
-  ledBlue();  delay(1000);
+  Serial.println("ESP8266 Online Status Push");
+  Serial.flush();
 
-  USE_SERIAL.begin(115200);
-  // USE_SERIAL.setDebugOutput(true);
+  Serial.println();
+  Serial.println();
 
-  USE_SERIAL.println();
-  USE_SERIAL.println();
-  USE_SERIAL.println();
+  for (uint8_t t = 4; t > 0; t--) {
+    Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.flush();
+    delay(1000);
+  }
 
-  USE_SERIAL.println("Start Wifi");
-  USE_SERIAL.flush();
   WiFi.mode(WIFI_STA);
-  WiFiMulti.addAP("Artifactory", "KEEP DOOR CLOSED");
-
+  WiFiMulti.addAP(SSID, PASSWORD);
 }
 
 void loop() {
   // wait for WiFi connection
   if ((WiFiMulti.run() == WL_CONNECTED)) {
 
-    ledBlue();
+    std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
 
-    HTTPClient http;
+    // Ignore the SSL certificate, we are just pushing status updates so it doesn't matter.
+    client->setInsecure();
 
-    USE_SERIAL.print("[HTTP] begin...\n");
-    // configure traged server and url
+    HTTPClient https;
 
-    http.begin(url, fingerprint); //HTTPS
-    //http.begin("http://google.com"); //HTTP
+    Serial.print("[HTTPS] begin...\n");
+    if (https.begin(*client, URL)) {  // HTTPS
 
-    USE_SERIAL.print("[HTTP] GET...\n");
-    // start connection and send HTTP header
-    int httpCode = http.GET();
+      Serial.print("[HTTPS] GET...\n");
+      // start connection and send HTTP header
+      int httpCode = https.GET();
 
-    // httpCode will be negative on error
-    if (httpCode > 0) {
-      // HTTP header has been send and Server response header has been handled
-      USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+      // httpCode will be negative on error
+      if (httpCode > 0) {
+        // HTTP header has been send and Server response header has been handled
+        Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
 
-      // file found at server
-      if (httpCode == HTTP_CODE_OK) {
-        ledGreen();
-        String payload = http.getString();
-        USE_SERIAL.println(payload);
+        // file found at server
+        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+          String payload = https.getString();
+          Serial.println(payload);
+        }
       } else {
-        ledRed();
+        Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
       }
-    } else {
-      ledRed();
-      USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-    }
 
-    http.end();
-  } else {
-    ledRed();
+      https.end();
+    } else {
+      Serial.printf("[HTTPS] Unable to connect\n");
+    }
   }
 
+  Serial.println("Wait 10s before next round...");
   delay(10000);
 }
